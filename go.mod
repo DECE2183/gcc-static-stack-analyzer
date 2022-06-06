dcea63c83122b7ccec62f3828e869febaa815901
Timofey Korolik <tim.korolik@gmail.com>
2022-06-06 16:55:42 +0300

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,3 @@
+module gcc-ssa
+
+go 1.18
---
+++ main.go
@@ -0,0 +1,121 @@
+package main
+
+import (
+  "fmt"
+  "os"
+  // "io"
+  "io/fs"
+  "regexp"
+  "strings"
+  "sort"
+  // "strconv"
+)
+
+const (
+  suFileRegexp = `[.]su$`
+  suFileNumbers = `[0-9]+[: ]*?`
+  suFileLine = `[a-zA-Z0-9:\s./_]+[\n]`
+)
+
+type StackCall struct {
+  line int
+  column int
+  memUsage int
+  fileName string
+  entryName string
+  qualifiers string
+}
+
+func stringUnspace(s string) string {
+  re, _ := regexp.Compile(`[a-zA-Z0-9:_/.-]+`)
+  strs := re.FindAllString(s, -1)
+  return strings.Join(strs, " ")
+}
+
+func stringUndrive(s string) string {
+  re, _ := regexp.Compile(`[A-Z]:[/\\]`)
+  if (re.MatchString(s)) {
+    return s[2:]
+  }
+  return s
+}
+
+func parseFile(calls *[]StackCall, str string) {
+  lines := strings.Split(strings.ReplaceAll(str, "\r\n", "\n"), "\n")
+  for _, line := range lines {
+    if (len(line) < 2) {
+      continue
+    }
+
+    var newCall StackCall;
+
+    line = stringUnspace(line)
+    line = stringUndrive(line)
+    line = strings.ReplaceAll(line, ":", " ")
+
+    fmt.Sscanf(line, "%s %d %d %s %d %s",
+      &newCall.fileName,
+      &newCall.line,
+      &newCall.column,
+      &newCall.entryName,
+      &newCall.memUsage,
+      &newCall.qualifiers)
+
+    *calls = append(*calls, newCall)
+  }
+}
+
+func main() {
+  _ = regexp.MustCompile(suFileRegexp)
+  _ = regexp.MustCompile(suFileNumbers)
+  _ = regexp.MustCompile(suFileLine)
+
+  if (len(os.Args) < 2) {
+    fmt.Println("You must provide a project path as an argument.")
+    os.Exit(22)
+  }
+
+  projPath := os.Args[1]
+  projFS := os.DirFS(projPath)
+  fmt.Printf("Project to analyze: %s\r\n", projPath)
+
+  // Find and analyze all .su files
+  var suFiles []string;
+  var suCalls []StackCall;
+  fs.WalkDir(projFS, ".", func (path string, dir fs.DirEntry, err error) error {
+    if (err != nil) {
+      return fs.SkipDir
+    }
+    match, _ := regexp.MatchString(suFileRegexp, path)
+    if (match) {
+      suFiles = append(suFiles, path)
+      fileContent, fError := os.ReadFile(fmt.Sprintf("%s/%s", projPath, path))
+      if (fError != nil) {
+        return fError
+      }
+      fileString := string(fileContent)
+      parseFile(&suCalls, fileString)
+    }
+    return nil
+  })
+
+  sort.SliceStable(suCalls, func(i, j int) bool {
+    return suCalls[i].memUsage > suCalls[j].memUsage
+  })
+
+  totalUsage := 0
+  for i, call := range suCalls {
+    fmt.Printf("% 3d: % 5d B %s->%s\r\n", i, call.memUsage, call.fileName, call.entryName)
+    totalUsage += call.memUsage
+  }
+  fmt.Printf("Total stack usage: %d Bytes\r\n", totalUsage)
+
+  if (len(suFiles) < 1) {
+    fmt.Println("There are no .su files.")
+    os.Exit(22)
+  }
+
+  // for _, f_name := range suFiles {
+  //   fmt.Println(f_name)
+  // }
+}
