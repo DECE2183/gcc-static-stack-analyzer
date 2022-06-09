962ac142ccd29f182b008f4cbd83a73287ba1c95
Timofey Korolik <tim.korolik@gmail.com>
2022-06-09 13:58:00 +0300

    Improved UI

--- gui.go
+++ gui.go
@@ -4,8 +4,8 @@ import (
 	"io"
 	"log"
 	"fmt"
-	"os"
-	"golang.org/x/term"
+	// "os"
+	// "golang.org/x/term"
 	// "time"
 	// "strings"
 	"github.com/charmbracelet/bubbles/list"
@@ -13,11 +13,18 @@ import (
 	"github.com/charmbracelet/lipgloss"
 )
 
+type modelPage uint
+const (
+	pageMain modelPage = 0
+	pageInfo modelPage = 1
+	pageQuit modelPage = 2
+)
 
 type model struct {
-	cursor int
 	stack *StackInfo
 	stackList list.Model
+
+	page modelPage
 }
 
 // List item interface
@@ -27,7 +34,7 @@ func (i StackCall) FilterValue() string {
 type callListItemDelegate struct{}
 
 func (d callListItemDelegate) Height() int {
-	return 1
+	return 3
 }
 func (d callListItemDelegate) Spacing() int {
 	return 0
@@ -41,7 +48,8 @@ func (d callListItemDelegate) Render(w io.Writer, m list.Model, index int, listI
 		return
 	}
 
-	str := fmt.Sprintf("%4d  %s %s %5d:%d", index + 1, listFileNameStyle.Render(item.fileName), listEntryNameStyle.Render(item.entryName), item.line, item.column)
+	line1 := fmt.Sprintf("%4d  %s %5d:%d", index + 1, listFileNameStyle.Render(item.fileName), item.line, item.column)
+	line2 := listEntryNameStyle.Render(fmt.Sprintf("-> %s ", item.entryName)) + listMemUsageStyle.Render(fmt.Sprintf("9%d B", item.memUsage))
 
 	var fn func(string) string
 
@@ -51,27 +59,28 @@ func (d callListItemDelegate) Render(w io.Writer, m list.Model, index int, listI
 		fn = itemStyle.Render
 	}
 
-	fmt.Fprintf(w, fn(str))
+	fmt.Fprintf(w, fn(lipgloss.JoinVertical(lipgloss.Top, line1, line2)))
 }
 
 var (
 	titleStyle        = lipgloss.NewStyle().
 												MarginLeft(2)
 	itemStyle         = lipgloss.NewStyle().
-												PaddingLeft(2)
+												PaddingLeft(2).
+												Border(lipgloss.NormalBorder(), false, false, true, false).
+										    BorderForeground(lipgloss.Color("#3C3C3C"))
 	selectedItemStyle = lipgloss.NewStyle().
-												PaddingLeft(1).
-												Border(lipgloss.Border{Left: "▋"}, false, false, false, true).
+												PaddingLeft(2).
+												Border(lipgloss.ThickBorder(), false, false, true, false).
 										    BorderForeground(lipgloss.Color("#22c2f2")).
-										    BorderBackground(lipgloss.Color("#07171c")).
-												Foreground(lipgloss.Color("#22c2f2")).
-												Background(lipgloss.Color("#07171c"))
-	listFileNameStyle = lipgloss.NewStyle().
-												MaxWidth(30).
-												Width(58)
+										    // BorderBackground(lipgloss.Color("#07171c")).
+												Foreground(lipgloss.Color("#22c2f2"))
+												// Background(lipgloss.Color("#07171c"))
+	listFileNameStyle = lipgloss.NewStyle()
 	listEntryNameStyle = lipgloss.NewStyle().
-												MaxWidth(30).
-												Width(34)
+												MarginLeft(6)
+	listMemUsageStyle = lipgloss.NewStyle().
+												Align(lipgloss.Right)
 
 	paginationStyle   = list.DefaultStyles().PaginationStyle.
 												PaddingLeft(4)
@@ -84,17 +93,20 @@ var (
 
 
 func startGUI(stack *StackInfo) {
-	physicalWidth, physicalHeigth, _ := term.GetSize(int(os.Stdout.Fd()))
+	// physicalWidth, physicalHeigth, _ := term.GetSize(int(os.Stdout.Fd()))
 
 	listItems := make([]list.Item, len(stack.calls))
 	for i, call := range stack.calls {
 		listItems[i] = call //callListItem(fmt.Sprintf("%s -> %s [%d:%d]", call.fileName, call.entryName, call.line, call.column))
 	}
 
-	m := model{0, stack, list.New(listItems, callListItemDelegate{}, physicalWidth, physicalHeigth - 2)}
+	m := model{
+		stack: stack,
+		stackList: list.New(listItems, callListItemDelegate{}, 512, 512),
+	}
 
 	m.stackList.Title = "Stack"
-	m.stackList.SetShowStatusBar(false)
+	m.stackList.SetShowStatusBar(true)
 	m.stackList.SetFilteringEnabled(false)
 	m.stackList.Styles.Title = titleStyle
 	m.stackList.Styles.PaginationStyle = paginationStyle
@@ -112,39 +124,72 @@ func (m model) Init() tea.Cmd {
 
 func (m model) Update(message tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := message.(type) {
+
+	case tea.WindowSizeMsg:
+		m.resize(msg.Width, msg.Height)
+		return m, nil
+
 	case tea.KeyMsg:
-		switch msg.String() {
-		case "q", "esc", "ctrl+c":
-			return m, tea.Quit
-		// case "up":
-		// 	m.cursor--
-		// 	if m.cursor < 0 {
-		// 		m.cursor = 0
-		// 	}
-		// case "down":
-		// 	m.cursor++
-		// 	if m.cursor >= len(m.stack.calls) {
-		// 		m.cursor = len(m.stack.calls) - 1
-		// 	}
+		keypress := msg.String()
+		switch m.page {
+
+		case pageMain:
+			switch keypress {
+			case "q", "esc", "ctrl+c":
+				m.page = pageQuit
+				return m, tea.Quit
+			case "enter":
+				m.page = pageInfo
+				return m, nil
+			}
+		
+			var cmd tea.Cmd
+			m.stackList, cmd = m.stackList.Update(message)
+			return m, cmd
+
+		case pageInfo:
+			switch keypress {
+			case "ctrl+c":
+				m.page = pageQuit
+				return m, nil
+			case "q", "esc", "enter":
+				m.page = pageMain
+				return m, nil
+			}
+			return m, nil
 		}
 	}
 
-	var cmd tea.Cmd
-	m.stackList, cmd = m.stackList.Update(message)
-	return m, cmd
+	return m, nil
 }
 
 func (m model) View() string {
-	physicalWidth, _, _ := term.GetSize(int(os.Stdout.Fd()))
+	// physicalWidth, physicalHeigth, _ := term.GetSize(int(os.Stdout.Fd()))
+	// m.resize(physicalWidth, physicalHeigth)
+	// m.stackList.SetWidth(physicalWidth)
+	// m.stackList.SetHeight(physicalHeigth - 2)
 
-	itemStyle = itemStyle.MaxWidth(physicalWidth - 2).Width(physicalWidth)
-	selectedItemStyle = selectedItemStyle.MaxWidth(physicalWidth - 2).Width(physicalWidth)
+	switch m.page {
+	case pageMain:
+		return "\n" + m.stackList.View()
+	case pageInfo:
+		return quitTextStyle.Render("Info.")
+	case pageQuit:
+		return quitTextStyle.Render("Not hungry? That’s cool.")
+	}
 
-	physicalWidth -= (26)
-	enamew := int(physicalWidth / 3)
-	fnamew := physicalWidth - enamew
-	listFileNameStyle = listFileNameStyle.MaxWidth(fnamew)
-	listEntryNameStyle = listEntryNameStyle.MaxWidth(enamew)
+	return ""
+}
 
-	return "\n" + m.stackList.View()
+func (m *model) resize(w, h int) {
+	itemStyle = itemStyle.MaxWidth(w - 2).Width(w)
+	selectedItemStyle = selectedItemStyle.MaxWidth(w - 2).Width(w)
+
+	namew := w - 26
+	listFileNameStyle = listFileNameStyle.MaxWidth(namew).Width(namew + 2)
+	listEntryNameStyle = listEntryNameStyle.MaxWidth(namew + 6).Width(namew + 8)
+
+	listMemUsageStyle.Width(8)
+
+	m.stackList.SetSize(w, h - 2)
 }
